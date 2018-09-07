ee3c0024a84060f954a528b37fec8d9dacb03139
Hugh Sorby <h.sorby@auckland.ac.nz>
2018-09-07 12:29:22 +1200

    Add developer version information to builds for Python.

--- bindings/python/opencmiss/iron/__init__.in.py
+++ bindings/python/opencmiss/iron/__init__.in.py
@@ -1,4 +1,3 @@
-# The iron_c.dll/iron.dll files are in the path where iron got installed to;
-# we need to add this to the PATH in order to have the import directive find the dependent dlls.
-import os
-os.environ['PATH'] = r'@NATIVE_LIBRARY_PATH@' + os.pathsep + os.environ['PATH']
+# Iron Python package initialisation file.
+__version__ = "@Iron_VERSION@@IRON_DEVELOPER_VERSION@"
+__revision__ = "@IRON_REVISION@"
